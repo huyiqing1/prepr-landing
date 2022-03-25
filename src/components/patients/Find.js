aab4882ecb1953d165fe75807a3d615a81520062
guwop <gauthaman.gnanasekaran@gmail.com>
2022-03-24 23:41:20 -0400

    NavBar, Find, and How it works has been finished for patients

--- src/components/patients/Find.js
+++ src/components/patients/Find.js
@@ -1,10 +1,29 @@
 import React from 'react'
-
+import {FaSearch} from 'react-icons/fa'
 const Find = () => {
   return (
     <section className="patients-section" id="patients-section-b">
         <div className="patients-content-wrap">
-        <h2>Find</h2>
+        <div className='patients-img-container'>
+          <div className='patients-sentence'>
+            <p className = "firstP">TheraPort makes it easy to get the therapy you want, whenever
+              you want
+            </p>
+            <p className = "secondP">Schedule a session now that fits in with your schedule</p>
+            {/* <div className="form-group has-search">
+              <span className="fa fa-search form-control-feedback"></span>
+              <input type="text" className="form-control" placeholder="Search"/>
+            </div> */}
+            <div class="wrap">
+              <div className="search">
+                  <input type="text" className="searchTerm" placeholder="What are you looking for?"/>
+                  <button type="submit" class="searchButton">
+                  <FaSearch/>
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
         </div>
     </section>
   )
--- src/components/patients/Navbar.js
+++ src/components/patients/Navbar.js
@@ -1,11 +1,19 @@
 import React from 'react'
-
+import logo from "../../images/logo.png"
+import "../../App.scss"
 const Navbar = () => {
   return (
     <section className="practitioners-section" id="practitioners-section-a">
         <div className="practitioners-content-wrap">
-
-        </div>
+          <div id='img-wrap'>
+          <img src={logo}></img>
+          </div>
+          <h2>TheraPortHealth</h2> 
+          <section>
+          <h5>For Practitioners</h5>
+          <h5>Sign up for free</h5>
+          </section>
+      </div>
     </section>
   )
 }
