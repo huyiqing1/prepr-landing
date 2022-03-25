b07e4c962e4cab76d06c7c58fc28e3bd2f9ebe36
huyiqing1 <huyiqing1@gmail.com>
2022-03-24 21:17:06 -0400

    commit

--- src/components/patients/Testimonial.js
+++ src/components/patients/Testimonial.js
@@ -4,7 +4,11 @@ const Testimonial = () => {
   return (
     <section className="patients-section" id="patients-section-e">
     <div className="patients-content-wrap">
-
+      <p>"As a new practitioner, I didn't know how to get into the field. 
+            Theraport Health helped me start 
+            my own business and now I have a huge client base that is growing everyday.
+            I would definitely recommend every practitioner to join this wonderful community." ~ <b>Boswell O.</b>
+            </p>
     </div>
   </section>
   )
