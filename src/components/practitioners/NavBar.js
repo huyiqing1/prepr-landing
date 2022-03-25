91e1e95014c8429fb61197480bc5dbe2c14799b6
huyiqing1 <huyiqing1@gmail.com>
2022-03-24 21:44:28 -0400

    updated navbar in patients

--- src/components/practitioners/NavBar.js
+++ src/components/practitioners/NavBar.js
@@ -7,12 +7,12 @@ const NavBar = () => {
     <section className="practitioners-section" id="practitioners-section-a">
       <div className="practitioners-content-wrap">
         <div id='img-wrap'>
-        <img src={logo}></img>
+          <img src={logo}></img>
         </div>
-        <h2>TheraPortHealth</h2> 
+        <h2>TheraPortHealth</h2>
         <section>
-        <h5>For Practitioners</h5>
-        <h5>Sign up for free</h5>
+          <h5>For Practitioners</h5>
+          <h5>Sign up for free</h5>
         </section>
       </div>
     </section>
